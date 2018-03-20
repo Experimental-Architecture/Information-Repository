09d79c547f8fdd5cf04975a053515503eb3f8d05
Christine <internalfire5@live.com>
2018-03-20 13:43:13 -0400

    Update minikube_startup.sh

--- minikube_startup.sh
+++ minikube_startup.sh
@@ -6,5 +6,10 @@ minikube stop
 ifdown ens33 && sudo ifup ens33
 rm /usr/local/minikube && rm -rf ~/.minikube
 minikube delete
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.23.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
-kubectl delete secret s
+
+#curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.23.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+# see the link to retrieve the releases: https://github.com/kubernetes/minikube/releases
+# latest release resolves the minikube start with the tcp error
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.25.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
